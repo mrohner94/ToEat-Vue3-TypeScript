c2922d9c3ccd27004142292a446b4501fb90b175
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-01-21 11:58:45 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import TheNavbar from './components/TheNavbar.vue'
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  components: {
+    TheNavbar,
+    TheFooter,
+  },
+}
+</script>
+
+<template>
+  <TheNavbar tagline="Track everything you want to eat" />
+  <RouterView />
+  <TheFooter />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Built with <a href="https://vuejs.org/">Vue</a> and <a href="https://bulma.io/">Bulma</a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style>
+
+</style>
